6f210820f22eae7a3c448b3d6e7b1ba5c1e3c4f9
SajibSaha207 <sajibsaha.207@gmail.com>
2025-08-28 12:45:11 +0600

    Added main js

---
+++ main.js
@@ -0,0 +1,83 @@
+function getElement(id) {
+  const elemnent = document.getElementById(id);
+  return elemnent;
+}
+
+//sidberContainer
+const sidberContainer = getElement("sidberCartContainer");
+
+//cardMethod heart-call-callHistory-copyBtn #multiple task inside in one method=========================
+getElement("card-container").addEventListener("click", function (e) {
+  //heart click and heat encrement
+  if (e.target.className.includes("fa-heart")) {
+    const heartCountDisply = getElement("heartCountDisply");
+    const heartCountDisplyconvert = parseInt(heartCountDisply.innerText);
+    heartCountDisply.innerText = heartCountDisplyconvert + 1;
+  }
+  //call button alert and show the cart info. decrement coin
+  if (e.target.className.includes("callBtn")) {
+    //coin
+    const coin = getElement("coinDisplay");
+    const coinNumber = parseInt(coin.innerText);
+    //current date get
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    if (coinNumber > 0) {
+      const callBtn = e.target;
+      const cardTitle = callBtn.parentNode.parentNode.children[1].innerText;
+      const contactCategory =
+        callBtn.parentNode.parentNode.children[2].innerText;
+      const contactNumber = callBtn.parentNode.parentNode.children[3].innerText;
+
+      alert(
+        `${cardTitle} \n 📞 calling ${contactCategory} ${contactNumber}...`
+      );
+      //coin decremt
+      let coinDecrementRes = coinNumber - 20;
+      coin.innerText = coinDecrementRes;
+
+      const newSidberCard = document.createElement("div");
+      newSidberCard.innerHTML = `
+          <div
+            class="flex justify-between p-1 md:p-3 bg-slate-200 rounded-md mt-2"
+          >
+            <div>
+              <h1 class="text-[10px] md:text-sm">${cardTitle}</h1>
+              <p class="text-gray-400 text-[10px] md:text-sm">${contactNumber}</p>
+            </div>
+            <div class="text-[10px] md:text-sm">${hours}:${minutes}:${seconds} ${ampm}</div>
+          </div>`;
+      sidberContainer.appendChild(newSidberCard);
+    } else {
+      alert(
+        "❌You don't have enough coins! You need at least 20 coins to make a call."
+      );
+    }
+  }
+
+  //copy button
+  if (e.target.className.includes("copyBtn")) {
+    const copyCounterDisply = getElement("copyCounterDisply");
+    const copyResConvertNum = parseInt(copyCounterDisply.innerText);
+    copyCounterDisply.innerText = copyResConvertNum + 1;
+    const cardInnerItems = e.target;
+    const cardTitle =
+      cardInnerItems.parentNode.parentNode.children[1].innerText;
+    const contactNumber =
+      cardInnerItems.parentNode.parentNode.children[3].innerText;
+    alert(` ${cardTitle} \n Number copied: ${contactNumber}`);
+
+    //copy number
+    navigator.clipboard.writeText(contactNumber);
+  }
+});
+
+//clearMethod button============================================
+const clearBtn = getElement("clearBtn");
+clearBtn.addEventListener("click", function () {
+  sidberContainer.innerHTML = "";
+});
